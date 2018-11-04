9df357d01c2e10cdc082c6342ea1f9d7eca8810f
yarik <yariks@i.ua>
2018-11-04 14:30:36 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("  ssssa".strip());
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
